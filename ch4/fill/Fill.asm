a8c8ccb36f98b3e0bb846fd5dc816b20ef76da30
pyjun01 <pyjun02@gmail.com>
2021-11-29 23:43:05 +0900

    ch4/fill

--- ch4/fill/Fill.asm
+++ ch4/fill/Fill.asm
@@ -1,14 +1,54 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+@index
+M=0
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(LOOP)
+  @KBD
+  D=M
 
-// Put your code here.
+	@FILL
+	D;JGT
+
+  @index
+  D=M
+
+  @LOOP
+  D;JEQ
+
+  // 키보드 안누르고 있으면서 idenx가 0보다 클 때
+
+  @index
+  M=M-1
+
+  @SCREEN
+  D=A
+
+  @index
+  D=D+M
+
+  A=D
+  M=0
+
+
+  @LOOP
+  0;JMP
+
+  (FILL)
+
+  @SCREEN
+  D=A
+
+  @index
+  D=D+M
+
+  A=D
+  M=-1
+
+  @index
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(END)
+	@END
+	0;JMP // 무한루프
